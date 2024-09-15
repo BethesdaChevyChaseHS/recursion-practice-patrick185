40035ef27cefc04f7e3d1516d704c021dfb7fd67
patrick185 <111162666+patrick185@users.noreply.github.com>
2024-09-15 19:35:38 +0000

    Saving my work!

--- src/main/java/com/example/project/RecursionPractice.java
+++ src/main/java/com/example/project/RecursionPractice.java
@@ -4,20 +4,38 @@ public class RecursionPractice {
 
 	public static void main(final String[] args) {
 		//use main to test your functions!
+		factorial(1);
+		sum(10);
+		fibonacci(9);
+		findTheFirstW("Hello World", 0);
 	}
 	
 	//factorial of n should return the long n multiplied by all numbers before it. 
 	//if n == 0 or n == 1, 1 should be returned.
 	//note that a long is just an int with twice the memory
 	public static long factorial(long n){
-		return 0;
+		long output = 1;
+
+		for (int i = 1; i<=n; i++) {
+			output = output*=i;
+			
+		}
+
+		
+		return output;
 	}
 
 	//sum of n should return the long n added  by all numbers before it. 
 	//if n == 0, 0 should be returned.
 	//this is essentially factorial but with addition
 	public static long sum(long n){
-		return 0;
+		long output = 0;
+
+		for (int i = 0; i<=n; i++) {
+			output+=i;
+			
+		}
+		return output;
 	}
 	
 	// The fibonacci of n should return the long value at position n in the Fibonacci sequence.
@@ -25,7 +43,13 @@ public class RecursionPractice {
 	// If n == 1, 1 should be returned.
 	// For n > 1, it should return the sum of fibonacci(n - 1) and fibonacci(n - 2).
 	public static long fibonacci(long n){
-		return 0;
+		if (n == 0) {
+			return 0;
+		}
+		else if (n==1) {
+			return 1;
+		}
+		return fibonacci(n-1) + fibonacci(n-2);
 	}
 
 	//Should return the index of the first W/w in a string. 
@@ -39,6 +63,19 @@ public class RecursionPractice {
 	//1)  the character at the current index is W
 	//2) The current index is out of bounds
 	public static int findTheFirstW(String s, int curIndex){
+		if (curIndex < 0 || curIndex>=s.length()) {
+			return -1;
+		}
+
+		for (int i = curIndex; i<s.length(); i++) {
+			
+			if (s.charAt(i) == 'w' || s.charAt(i) == 'W') {
+				System.out.println(i);
+				return i;
+				
+			}
+		}
+		
 		return -1;
 	}
 }
