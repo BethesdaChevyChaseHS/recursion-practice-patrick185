51f4efb071ede9951f4b1d65d45916f48428759b
patrick185 <111162666+patrick185@users.noreply.github.com>
2024-09-20 11:55:36 +0000

    Saving my work

--- src/main/java/com/example/project/RecursionPractice.java
+++ src/main/java/com/example/project/RecursionPractice.java
@@ -4,38 +4,35 @@ public class RecursionPractice {
 
 	public static void main(final String[] args) {
 		//use main to test your functions!
-		factorial(1);
-		sum(10);
+		System.out.println(factorial(5));
+		System.out.println(sum(10));
+		System.out.println(findTheFirstW("Hello world", 0));
 		fibonacci(9);
-		findTheFirstW("Hello World", 0);
+		findTheFirstW("Hello World", 1);
 	}
 	
 	//factorial of n should return the long n multiplied by all numbers before it. 
 	//if n == 0 or n == 1, 1 should be returned.
 	//note that a long is just an int with twice the memory
 	public static long factorial(long n){
-		long output = 1;
-
-		for (int i = 1; i<=n; i++) {
-			output = output*=i;
-			
+		if (n==1 || n==0) {
+			return 1;
+		}	
+		else {
+			return n*factorial(n-1);
 		}
-
-		
-		return output;
 	}
 
 	//sum of n should return the long n added  by all numbers before it. 
 	//if n == 0, 0 should be returned.
 	//this is essentially factorial but with addition
 	public static long sum(long n){
-		long output = 0;
-
-		for (int i = 0; i<=n; i++) {
-			output+=i;
-			
-		}
-		return output;
+		
+		if (n<=0) {
+			return n;
+		}	
+		return n+sum(n-1);
+		
 	}
 	
 	// The fibonacci of n should return the long value at position n in the Fibonacci sequence.
@@ -66,17 +63,16 @@ public class RecursionPractice {
 		if (curIndex < 0 || curIndex>=s.length()) {
 			return -1;
 		}
-
-		for (int i = curIndex; i<s.length(); i++) {
+		if (s.charAt(curIndex) == 'W' || s.charAt(curIndex) == 'w') {
 			
-			if (s.charAt(i) == 'w' || s.charAt(i) == 'W') {
-				System.out.println(i);
-				return i;
-				
-			}
+			return curIndex;
 		}
+		return findTheFirstW(s, curIndex+1);
+	
 		
-		return -1;
+		
+		
+		
 	}
 }
 
